9af3b4d1e12f7659b50daf116e96f7ab7e0f0bd3
raivimo <raimonv@gmail.com>
2023-02-06 19:37:58 +0100

    mejoras

--- src/main/java/com/raimon/dogfriendly/service/PerroService.java
+++ src/main/java/com/raimon/dogfriendly/service/PerroService.java
@@ -94,7 +94,7 @@ public class PerroService {
         }
     }
 
-    public Page<PerroEntity> getPage(Pageable oPageable, String strFilter, Long id_raza, Long id_usuario) {
+    public Page<PerroEntity> getPage(Pageable oPageable, String strFilter, Long id_usuario, Long id_raza) {
         // oAuthService.OnlyAdmins();
         ValidationHelper.validateRPP(oPageable.getPageSize());
         Page<PerroEntity> oPage = null;
